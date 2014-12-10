60f2597f68aa8825a56375557ab7e1bee612bfe3
JBe <bednarik@live.com>
2014-12-10 03:35:43 +0100

    Changed default namespace in ContinuousMemoryStream.Tests project. Fixed typo.

--- src/ContinuousMemoryStream.Tests/ContinuousMemoryStreamTests.cs
+++ src/ContinuousMemoryStream.Tests/ContinuousMemoryStreamTests.cs
@@ -12,7 +12,7 @@ namespace JBe.IO.Tests
         private Thread writeThread;
         private Thread readThread;
 
-        private const string data =
+        private const string Data =
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu lobortis justo, ut tempor sapien. Donec quam neque, tincidunt a gravida at, tempus sed dui. Etiam nec vehicula erat. Nullam semper semper pulvinar. Integer eleifend neque elit, vitae ultrices eros accumsan in. Fusce pharetra euismod neque. Suspendisse accumsan augue justo, at dictum lorem dignissim consequat. Quisque rutrum urna ante, id commodo ipsum accumsan non. Aliquam efficitur ante volutpat molestie pulvinar. In risus enim, euismod malesuada efficitur at, accumsan id ipsum. Suspendisse volutpat, nunc ut lacinia varius, risus sem semper purus, id scelerisque diam nisl congue augue. Maecenas interdum nibh sit.";
 
         [Fact]
@@ -26,7 +26,7 @@ namespace JBe.IO.Tests
             int readtotal = 0;
             writeThread = new Thread(() =>
             {
-                var bytes = Encoding.UTF8.GetBytes(data);
+                var bytes = Encoding.UTF8.GetBytes(Data);
                 stream.Write(bytes, 0, bytes.Length);
                 writtentotal = bytes.Length;
                 stream.Dispose();
@@ -58,7 +58,7 @@ namespace JBe.IO.Tests
             readThread.Join();
 
             Assert.Equal(writtentotal, readtotal);
-            Assert.Equal(data, receivedData);
+            Assert.Equal(Data, receivedData);
         }
 
         public void Dispose()
